37a8e6c715e11647d14033bd9df4ff8db86e5f09
skolldev <hello@alexandermay.dev>
2020-06-20 17:48:32 +0200

    email

---
+++ api/send-email.js
@@ -0,0 +1,14 @@
+module.exports = (req, res) => {
+  const { password } = req.body;
+  const sg = require("@sendgrid/mail");
+  sg.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "test@example.com",
+    from: "test@example.com",
+    subject: "Sending with Twilio SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sg.send(msg);
+  res.send("Success");
+};
--- src/components/CoachingInquiry.jsx
+++ src/components/CoachingInquiry.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const CoachingInquiry = () => {
+  const email = async () => {
+    const response = await fetch("/api/send-email", {
+      method: "POST",
+      mode: "cors",
+    });
+    console.log(response);
+  };
   return (
     <form
       id="coaching-inquiry"
@@ -100,6 +107,7 @@ const CoachingInquiry = () => {
             <button
               type="button"
               className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
+              onClick={() => email}
             >
               <svg
                 className="-ml-1 mr-3 h-5 w-5"
