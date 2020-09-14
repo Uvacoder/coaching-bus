a7151d773fef4caf843787fa5d37049fbb1c9c5a
Alexander May <alexander.may@spendit.de>
2020-09-14 15:11:58 +0200

    fix markdown loading bug

--- src/templates/template.jsx
+++ src/templates/template.jsx
@@ -92,7 +92,7 @@ export default function Template({ data }) {
               <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl text-center">
                 {frontmatter.title}
               </h2>
-              <p
+              <div
                 className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
                 id="markdown-content"
                 dangerouslySetInnerHTML={{ __html: html }}
